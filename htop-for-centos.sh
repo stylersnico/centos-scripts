1eda871f0c286c3e2a644b517ab5bdb6b15c7ac1
Nicolas Simond <stylersnico@gmail.com>
2014-06-26 14:39:47 +0200

    Update htop-for-centos.sh

--- htop-for-centos.sh
+++ htop-for-centos.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+# Use : wget -O- https://raw.githubusercontent.com/stylersnico/centos-scripts/master/htop-for-centos.sh | sh
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 echo "--> Download the repository file"
 cd /etc/yum.repos.d/
 wget https://raw.githubusercontent.com/stylersnico/centos-scripts/master/config-files/CentOS-Dag.repo
