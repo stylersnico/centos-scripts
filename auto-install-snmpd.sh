1f6e4e4b674b8e60109e2739b60e308f24d5e67e
Nicolas Simond <contact@nicolas-simond.com>
2014-06-28 13:57:19 +0200

    Auto Install snmpd

---
+++ auto-install-snmpd.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# Use : wget -O- https://raw.githubusercontent.com/stylersnico/centos-scripts/master/auto-install-snmpd.sh | sh
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "--> Updating CentOS System"
+yum -y update
+
+echo "--> Installing htop"
+yum -y install net-snmp
+
+echo "--> Launch snmpd at boot"
+chkconfig --level 3 snmpd on
+
+echo "--> Restart services"
+/etc/init.d/snmpd stop && /etc/init.d/snmpd start
